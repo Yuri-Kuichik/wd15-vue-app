74927ca4719eebddd9432c87cbe44ec974ed4ab3
Yuri Kuichik <kuichikmis4enko@gmail.com>
2025-03-26 22:34:55 +0300

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<script>
+import BaseLayout from '@/components/BaseLayout.vue';
+
+export default {
+  components: {
+    BaseLayout
+  },
+
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <BaseLayout>
+    <button @click="count++">Вы нажали на меня {{ count }} раз.</button>
+  </BaseLayout> 
+</template>
